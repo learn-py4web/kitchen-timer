f714d67427711a15286ffac25f70ea88a4b85c3b
Luca de Alfaro <luca@dealfaro.com>
2024-10-07 18:10:58 -0700

    oh well

--- index.js
+++ index.js
@@ -18,50 +18,10 @@ app.data = {
     methods: {
         plus: function() {
             this.minutes++;
-            if (!this.counter) {
-                this.seconds = 0;
-            }
         },
-        minus: function () {
-            if (this.minutes > 0) {
-                this.minutes--;
-            }
-            if (!this.counter) {
-                this.seconds = 0;
-            }
+        minus: function() {
+            this.minutes = Math.max(0, this.minutes - 1);
         },
-        play: function() {
-            let self = this;
-            if (!this.counter && (this.minutes > 0 || this.seconds > 0)) {
-                this.counter = setInterval(function() {
-                    self.seconds--;
-                    if (self.seconds < 0) {
-                        self.seconds = 59;
-                        self.minutes--;
-                    }
-                    if (self.minutes === 0 && self.seconds === 0) {
-                        self.ring();
-                        clearInterval(self.counter);
-                        self.counter = null;
-                    }
-                }, 200);
-            }
-        },
-        pause: function() {
-            if (this.counter) {
-                clearInterval(this.counter);
-                this.counter = null;
-            }
-        },
-        stop: function() {
-            this.pause();
-            this.minutes = 0;
-            this.seconds = 0;
-        },
-        ring: function() {
-            let audio = new Audio("ring.mp3");
-            audio.play();
-        }
     }
 };
 
