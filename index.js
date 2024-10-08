5bbf925e80da33b4b9a431f86abfe14937f2c419
Luca de Alfaro <luca@dealfaro.com>
2024-10-07 18:46:35 -0700

    end of class

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.data = {
             minutes: 0,
             seconds: 0,
             counter: null,
+            count_up: null,
         };
     },
     computed: {
@@ -22,6 +23,50 @@ app.data = {
         minus: function() {
             this.minutes = Math.max(0, this.minutes - 1);
         },
+        reset: function() {
+            this.pause();
+            this.minutes = 0;
+            this.seconds = 0;
+            this.count_up = null;
+        },
+        play: function() {  
+            let self = this;
+            if (!this.counter) {
+                if (self.count_up || (this.minutes == 0 && this.seconds == 0))
+                {
+                    this.count_up = true;
+                    this.counter = setInterval(function () {
+                        self.seconds++;
+                        if (self.seconds > 59) {
+                            self.seconds = 0;
+                            self.minutes++;
+                        }
+                    }, 100);
+                }
+                else
+                { 
+                    this.count_up = false;               
+                    this.counter = setInterval(function () {
+                        if (self.seconds > 0 || self.minutes > 0) {
+                            self.seconds--;
+                            if (self.seconds < 0) {
+                                self.seconds = 59;
+                                self.minutes--;
+                            }
+                        } else {
+                            clearInterval(self.counter);
+                            self.counter = null;
+                        }
+                    }, 100);
+                } 
+            }
+        },
+        pause: function() {
+            if (this.counter) {
+                clearInterval(this.counter);
+                this.counter = null;
+            }
+        }
     }
 };
 
